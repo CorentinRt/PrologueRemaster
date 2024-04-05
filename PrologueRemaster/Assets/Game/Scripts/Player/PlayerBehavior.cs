e1f6de6df7295fbf04102e1e66e5d3a440b456f6
CorentinRmt <corentin.remot@gmail.com>
2024-04-06 01:56:21 +0200

    Player move and jump

---
+++ PrologueRemaster/Assets/Game/Scripts/Player/PlayerBehavior.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    [Header("Move brain")]
+
+    [SerializeField] private Transform _topWallCheck;
+    [SerializeField] private Transform _botWallCheck;
+
+    [Header("Jump brain")]
+
+    [SerializeField] private Transform _leftGroundCheck;
+    [SerializeField] private Transform _rightGroundCheck;
+
+    [SerializeField] private LayerMask _groundLayerMask;
+
+    public bool GroundCheck()
+    {
+        if (Physics2D.OverlapArea(_leftGroundCheck.position, _rightGroundCheck.position, _groundLayerMask))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool WallCheck()
+    {
+        if (Physics2D.OverlapArea(_topWallCheck.position, _botWallCheck.position, _groundLayerMask))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public float GetLookDirection()
+    {
+        return transform.localScale.x;
+    }
+    public void SetLookDirection(float direction)
+    {
+        if (direction == 0f)
+        {
+            return;
+        }
+        if (direction > 0f)
+        {
+            Vector2 tempVect = new Vector2(1f, transform.localScale.y);
+            transform.localScale = tempVect;
+        }
+        else
+        {
+            Vector2 tempVect = new Vector2(-1f, transform.localScale.y);
+            transform.localScale = tempVect;
+        }
+    }
+}
---
+++ PrologueRemaster/Assets/Game/Scripts/Player/PlayerMovements.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovements : MonoBehaviour
+{
+    #region Fields
+    private PlayerBehavior _playerBehavior;
+
+    [SerializeField] private InputActionReference _move;
+
+    [SerializeField] private InputActionReference _jump;
+
+    private Rigidbody2D _rb2D;
+
+    private Vector2 _moveVector;
+
+    [SerializeField] private float _moveSpeed;
+
+    [SerializeField] private float _jumpForce;
+
+    #endregion
+
+    private void Start()
+    {
+        _playerBehavior = transform.parent.GetComponent<PlayerBehavior>();
+
+        _rb2D = transform.parent.GetComponent<Rigidbody2D>();
+
+        _move.action.started += StartMove;
+
+        _move.action.performed += UpdateMove;
+
+        _move.action.canceled += StopMove;
+
+        _jump.action.started += Jump;
+    }
+    private void OnDestroy()
+    {
+        _move.action.started -= StartMove;
+
+        _move.action.performed -= UpdateMove;
+
+        _move.action.canceled -= StopMove;
+
+        _jump.action.started -= Jump;
+    }
+
+    private void FixedUpdate()
+    {
+        float tempVelcityY = _rb2D.velocity.y;
+
+        Vector2 tempVector = _rb2D.velocity;
+
+        tempVector = _moveSpeed * _moveVector * Time.fixedDeltaTime;
+        tempVector.y = tempVelcityY;
+
+        _rb2D.velocity = tempVector;
+    }
+
+    private void StartMove(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    private void UpdateMove(InputAction.CallbackContext context)
+    {
+        _playerBehavior.SetLookDirection(context.ReadValue<Vector2>().x);
+
+        if (!_playerBehavior.WallCheck())
+        {
+            _moveVector = context.ReadValue<Vector2>();
+        }
+        else
+        {
+            if (_playerBehavior.GetLookDirection() > 0f && context.ReadValue<Vector2>().x > 0f)
+            {
+                _moveVector = Vector2.zero;
+            }
+            else if (_playerBehavior.GetLookDirection() < 0f && context.ReadValue<Vector2>().x < 0f)
+            {
+                _moveVector = Vector2.zero;
+            }
+        }
+    }
+
+    private void StopMove(InputAction.CallbackContext context)
+    {
+        _moveVector = Vector2.zero;
+    }
+
+    private void Jump(InputAction.CallbackContext context)
+    {
+        if (_playerBehavior.GroundCheck())
+        {
+            _rb2D.AddForce(transform.TransformDirection(Vector3.up) * _jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
